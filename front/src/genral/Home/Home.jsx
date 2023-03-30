1eda8196d141552c3c05132371b13712519be888
MarkGrinberg1911 <mark.grinberg703@gmail.com>
2023-03-30 12:31:35 +0300

    i hate desgining things

--- front/src/genral/Home/Home.jsx
+++ front/src/genral/Home/Home.jsx
@@ -2,10 +2,21 @@
 function Home(props) {
     return (
         //hero div
-        <div className="flex w-screen h-screen">
-            <div className="p-6  h-1/6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-                <p className="text-2xl font-mono font-bold underline underline-offset-8 ">Hello and Welcome To TicTacToad</p>
+        <div className="flex-wrap w-screen h-screen antialiased  dark:text-slate-400 bg-slate-900">
+            <div className="p-6  h-min w-1/3 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 justify-center">
+                <p className="text-2xl font-mono font-bold underline underline-offset-8 ">
+                    Hello and Welcome To TicTacTo
+                </p>
             </div>
+            <div className="flex items-center h-min  justify-center">
+                <div className="p-6  h-min w-min mx-auto text-white rounded-xl bg- shadow-slate-200  shadow-2xl flex items-center space-x-4">
+                    <p className="text-2xl font-mono font-bold underline underline-offset-8 "> PVP</p>
+                </div>
+                <div className="p-6  h-min w-min mx-auto bg-white rounded-xl shadow-lg  space-x-4">
+                    <p className="text-2xl font-mono font-bold underline underline-offset-8 "> PVE</p>
+                </div>
+            </div>
+
         </div>
     );
 }
